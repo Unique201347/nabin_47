912c80305fd270038900c5345f7380d51a920587
Nabindhami03 <nabindhami619@gmail.com>
2023-07-05 20:09:51 +0545

    feat: transistions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
 	import '../app.css';
 	import ThemeSwitch from '../lib/components/ThemeSwitch.svelte';
+	import PageTransition from './ransition.svelte';
+
+	export let data;
 </script>
 
 <ThemeSwitch />
-<slot />
+
+<PageTransition url={data.url}>
+	<slot />
+</PageTransition>
